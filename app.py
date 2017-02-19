e6165a51e41cd48458409a98cb82f5c64acc0699
alexpepperb <alexbingham1@hotmail.com>
2017-02-19 23:05:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
+    city = parameters.get("uriartist")
     if city is None:
         return None
 
