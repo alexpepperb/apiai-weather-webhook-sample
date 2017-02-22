3c813d93f9526f554b1458a05c1356a2c5f3e12a
alexpepperb <alexbingham1@hotmail.com>
2017-02-22 23:43:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def webhook():
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
+    r.headers['Accept'] = 'application/json'
     return r
 
 
